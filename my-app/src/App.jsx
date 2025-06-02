15621dfc80db00c40506b8fe9ebb8fd78a00ba9c
Piyush Rawat <unexpected086@gmail.com>
2025-06-02 11:53:04 +0530

    Landing Page

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import KumbaAILanding from './components/KumbaAILanding'
+
+function App() {
+  return (
+    <div className="h-screen w-screen overflow-hidden">
+      <KumbaAILanding />
+    </div>
+  )
+}
+
+export default App
